706244c874868c6438901646cd34724f25f27226
Nick Olenz <nickolenz@comcast.net>
2016-11-12 16:09:49 -0600

    Reset

---
+++ Assets/Scripts/ToggleButton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ToggleButton : MonoBehaviour
+{
+	public bool toggled = false;
+
+	Button ourButton;
+	Image ourButtonImage;
+
+	// Use this for initialization
+	void Start ()
+	{
+		ourButton = GetComponent<Button>();
+		ourButtonImage = GetComponent<Image>();
+		ourButton.onClick.AddListener(() =>
+		{
+			if (toggled)
+			{
+				ourButtonImage.color = Color.white;
+			}
+			else
+			{
+				ourButtonImage.color = Color.green;
+			}
+			toggled = !toggled;
+		});
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # 465App
-An implementation of our group project. Created in Unity.
+An implementation of our group project. Created in Unity 5.3.1f. (Probably works in later versions as well, but can't make any promises.)
