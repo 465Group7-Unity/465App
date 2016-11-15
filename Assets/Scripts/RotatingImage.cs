66761ba3059ebabff218897b0354bbdbfd4d5916
Nick Olenz <nickolenz@comcast.net>
2016-11-15 00:00:24 -0600

    Done enough now.

--- Assets/Scripts/RotatingImage.cs
+++ Assets/Scripts/RotatingImage.cs
@@ -6,11 +6,14 @@ public class RotatingImage : MonoBehaviour
 {
 	Image ourImage;
 	float oldSign = 1;
+	Sprite[] faces;
+	int idx = 0;
 
 	// Use this for initialization.
 	void Start ()
 	{
 		ourImage = GetComponent<Image>();
+		faces = Resources.LoadAll<Sprite>("");
 	}
 
 	// FixedUpdate is called 60 times per second.
@@ -20,7 +23,10 @@ public class RotatingImage : MonoBehaviour
 		if (Mathf.Sign(transform.localScale.x) != oldSign)
 		{
 			//Debug.Log("FLIP");
-			ourImage.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+			ourImage.sprite = faces[idx % faces.Length];
+			idx++;
+
+			//ourImage.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
 		}
 		oldSign = Mathf.Sign(transform.localScale.x);
 	}
