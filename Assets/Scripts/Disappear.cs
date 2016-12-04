eeba57eb7c4bb87d869b5a7872d11640b70e6fb5
Nick Olenz <nickolenz@comcast.net>
2016-12-04 16:47:59 -0600

    Really finished.

---
+++ Assets/Scripts/Disappear.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Disappear : MonoBehaviour
+{
+	public Text txt;
+	public Image arrow;
+
+	public ToggleButton[] cuisines;
+	bool alive = true;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (alive)
+		{
+			foreach (ToggleButton ii in cuisines)
+			{
+				if (ii.toggled)
+				{
+					alive = false;
+					txt.enabled = false;
+					arrow.enabled = false;
+					break;
+				}
+			}
+         }
+	}
+}
